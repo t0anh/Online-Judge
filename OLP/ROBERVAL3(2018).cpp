f67a6e15852412fe21960190b4e69c4d80b56a10
Nguyễn Tuấn Anh <tuananhcnt55vmu@gmail.com>
2019-06-23 21:48:38 +0700

    Update ROBERVAL3(2018).cpp

--- OLP/ROBERVAL3(2018).cpp
+++ OLP/ROBERVAL3(2018).cpp
@@ -1,58 +1,57 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
-typedef long long llong;
 
-llong power[40];
+const int MAX_EXP = 38;
+
+long long acc[40];
+long long power3[40];
 int cnt[40];
 
+void init () {
+    acc[0] = 1;
+    power3[0] = 1;
+    for (int i = 1; i <= MAX_EXP; ++i) {
+        power3[i] = power3[i-1] * 3;
+        acc[i] = acc[i-1] + power3[i];
+    }
+} 
+
 int main() {
-	freopen("input.txt", "r", stdin);
-//	freopen("output.txt", "w", stdout);
 	
-	power[0] = 1;
-	for (int i = 1; i < 40; ++i) {
-		power[i] = power[i-1] * 3;
-	}
-	
-	llong M;
-	
-	cin >> M;
-	
-	llong sum = 0;
-	int e = 0;
-	while(sum < M) {
-		sum += power[e];
-		cnt[e]++; 
-		++e;
-	}
-	
-	llong rem = sum - M;
-	e = 39;
-	while(rem > 0) {
-		while(power[e] > rem) {
-			--e;
-		}
-		int x = rem / power[e];
-		cnt[e] -= x;
-		rem -= x * power[e];
-	}
-	bool empty = true;
-	for (int i = 0; i < 40; ++i) {
-		if (cnt[i] == -1) {
-			empty = false;
-			cout << i << " ";
-		}
-	}
-	if (empty) {
-		cout << "-1";
-	}
-	cout << endl;
-	for (int i = 0; i < 40; ++i) {
-		if (cnt[i] == 1) {
-			cout << i << " ";
-		}
-	}
+    init();
+
+    long long m;
+
+    cin >> m;
+
+    for (int i = 0; i <= MAX_EXP; ++i) {
+        cnt[i]++;
+    }
+
+    long long rem = acc[MAX_EXP] - m;
+    while (rem != 0) {
+        for (int i = MAX_EXP; i >= 0; --i) {
+            if (power3[i] <= rem) {
+                rem -= power3[i];
+                cnt[i]--;
+                break;
+            }
+        }
+    }
+    
+    bool empty = true;
+    for (int i = 0; i <= MAX_EXP; ++i) {
+        if (cnt[i] < 0) {
+            cout << i << " ";
+            empty = false;
+        }
+    }
+    puts(empty ? "-1" : "");
+    for (int i = 0; i <= MAX_EXP; ++i) {
+        if (cnt[i] > 0) {
+            cout << i << " ";
+        }
+    }
 	return 0;
 }
-
